11daf217ade24b1786cc321fcadadf083cb018cf
cthrnsdlr <catherine.sadler3@nhs.net>
2022-11-16 11:49:23 +0000

    merge main

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -209,7 +209,7 @@ describe("/api/reviews/:review_id/comments", () => {
     .send(newCommentNoBody)
     .expect(400)
     .then(res => {
-      expect(res.body.msg).toBe('Incomplete object')
+      expect(res.body.msg).toBe('Incomplete object on body')
   })
   });
   test('POST 400 - bad request if object doesnt have all required keys', () => { 
@@ -221,7 +221,7 @@ describe("/api/reviews/:review_id/comments", () => {
     .send(newCommentNoUsername)
     .expect(400)
     .then(res => {
-      expect(res.body.msg).toBe('Incomplete object')
+      expect(res.body.msg).toBe('Incomplete object on body')
   })
   })
   test('POST 400 - errors for review_id not of type integer', () => {
